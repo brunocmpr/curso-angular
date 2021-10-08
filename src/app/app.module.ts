ca62fe30fddcf0140d2ed4269b52b5ac8b13549b
Bruno Campera <bruno.campera@serpro.gov.br>
2021-10-08 09:31:23 -0300

    Cria tela de 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorsModule } from './errors/errors.module';
 import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
@@ -13,6 +14,7 @@ import { PhotosModule } from './photos/photos.module';
     BrowserModule,
     AppRoutingModule,
     PhotosModule,
+    ErrorsModule,
   ],
   providers: [ ],
   bootstrap: [AppComponent]
